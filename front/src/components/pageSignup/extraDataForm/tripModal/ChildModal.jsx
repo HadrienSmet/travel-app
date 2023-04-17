cfa8fe77ea21fdd987b8b3382da85ab9a5ae08d3
Hadrien <smethadrien.96@gmail.com>
2023-04-17 16:49:05 +0200

    last commit before deployment... fingers crossed dont want to go through the same problems than when I deployed the gartic clone app

--- front/src/components/pageSignup/extraDataForm/tripModal/ChildModal.jsx
+++ front/src/components/pageSignup/extraDataForm/tripModal/ChildModal.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, useState } from "react";
 
 import { useDispatch } from "react-redux";
 import { setAlbumObjectArrayStore } from "../../../../features/albumObjectArray.slice";
